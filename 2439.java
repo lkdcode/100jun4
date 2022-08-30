aaefddcc5c77de8cca46688379f115d2ed777230
lkdcode <110602069+lkdcode@users.noreply.github.com>
2022-08-30 23:48:02 +0900

    beakjoon.java.conditional statement

---
+++ 2439.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+		int a = scan.nextInt();
+
+		for (int i = 1; i <= a; i++) {
+
+			for (int j = 1; j <= a - i; j++) {
+				System.out.printf(" ");
+			}
+			for (int k = 1; k <= i; k++) {
+				System.out.printf("*");
+			}
+			System.out.println();
+		}
+    }
+}
